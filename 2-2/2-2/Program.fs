1f5ff1528c9a296a99a624e79d6073923d5eaaf5
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-03-02 18:06:06 +0300

    add 2-2

---
+++ 2-2/2-2/Program.fs
@@ -0,0 +1,11 @@
+﻿let list = [1; 2; 3; 4; 5]
+
+let search  list number = 
+    let rec help list index =
+            if (List.length list > 0) then
+                if (List.head list = number) then index
+                    else help (List.tail list) (index + 1)
+            else -228
+    help list 1
+
+printfn "position of 4 = %A" <| search  list 4
