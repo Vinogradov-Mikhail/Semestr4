da4fdd3146a8bee3fa900ce4f8d6755cbcc7ef74
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-03-02 17:04:23 +0300

    add 2-1

---
+++ 2-1/2-1/Program.fs
@@ -0,0 +1,14 @@
+﻿let numberMultiply x =
+    let rec helpMultiply number =
+        if number < 10 then number
+        else helpMultiply (number / 10) * (number % 10)
+    helpMultiply x
+
+let checkOne = numberMultiply 123
+printfn "123 = %d" checkOne 
+
+let checkTwo = numberMultiply 3
+printfn "3 = %d" checkTwo 
+
+let checkThree = numberMultiply 341111
+printfn "341111 = %d" checkThree 
