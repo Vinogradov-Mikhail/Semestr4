452ca43ef905c1d4e313d467e7831a1845646746
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-03-05 13:21:05 +0300

    add "match"

--- 2-2/2-2/Program.fs
+++ 2-2/2-2/Program.fs
@@ -1,11 +1,11 @@
-﻿let list = [1; 2; 3; 4; 5]
+﻿let list = []
 
 let search  list number = 
-    let rec help list index =
-            if (List.length list > 0) then
-                if (List.head list = number) then index
-                    else help (List.tail list) (index + 1)
-            else -228
+    let rec help list (index : int) =
+        match list with
+        |[] -> None
+        |_ when List.head list = number -> Some(index)
+        |head::tail -> help (List.tail list) (index + 1)
     help list 1
 
 printfn "position of 4 = %A" <| search  list 4
