f8ee5661d60caaf4e92e050f57aecf685446d2a3
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-03-12 16:18:19 +0300

    add 3-1

---
+++ 3-1/3-1/Program.fs
@@ -0,0 +1,10 @@
+﻿let rec areElementsDifferentInList list =
+    match list with
+        | [] -> true
+        | head :: tail ->
+            if List.exists (fun n -> n = head) tail then false
+            else areElementsDifferentInList tail
+
+let ls = [1; 2; 2; 4]
+
+printfn"elements is different? - %A" <| areElementsDifferentInList ls
