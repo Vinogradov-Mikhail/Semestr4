5d8587a43e58e466406036b8af02cc123ac0a2a5
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-03-13 23:23:37 +0300

    update algoritm

--- 2-4/2-4/Program.fs
+++ 2-4/2-4/Program.fs
@@ -1,19 +1,19 @@
 ﻿let rec mergeSort list = 
-    let rec merge left right =
+    let rec merge left right result=
         match left, right with
-        | [], [] -> []
-        | left, [] -> left
-        | [], right -> right
-        | leftHead::leftTail, rihgtHead::rightTail -> 
-            if leftHead <= rihgtHead then leftHead::merge leftTail right
-            else rihgtHead::merge left rightTail  
+            | [], [] -> List.rev result
+            | leftHead::leftTail, [] -> merge leftTail [] (leftHead::result)
+            | [], rightHead::rightTail ->  merge rightTail [] (rightHead::result)
+            | leftHead::leftTail, rightHead::rightTail -> 
+                if leftHead <= rightHead then merge leftTail right (leftHead::result)
+                else merge left rightTail (rightHead::result)
     match list with
         | [] -> []
         | a::[] -> [a]
         | list ->
             let left, right = List.splitAt(List.length list / 2) list
-            merge (mergeSort left) (mergeSort right)
+            merge (mergeSort left) (mergeSort right) []
 
-let list = [2; 1; 9; 2; 1]
+let list = List.init 10000 (fun i -> -i)
 
 printfn "sorted list %A" <| mergeSort list
