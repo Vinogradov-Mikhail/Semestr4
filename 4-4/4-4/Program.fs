76c4991cbaf956d8ffc885ab1fa988c1a778a801
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-03-22 22:54:46 +0300

    add 4-4

---
+++ 4-4/4-4/Program.fs
@@ -0,0 +1,12 @@
+﻿let check x =
+    let rec help y n acc=
+        match n with
+        | 1 -> acc
+        | _ -> 
+            if (y % n = 0) then help y 1 false
+            else  help y (n - 1) acc
+    help x (x / 2) true
+
+let square = Seq.initInfinite (fun index -> index + 2)
+let primeSquare =  Seq.filter(fun x -> check x) square
+printfn "%A" primeSquare
