b3c80833365c10d8e6c164c210b0b50551a61240
Vinogradov-Mikhail <vinogradov97m@gmail.com>
2017-03-22 22:01:05 +0300

    Update Program.fs

--- 4-2/4-2/Program.fs
+++ 4-2/4-2/Program.fs
@@ -11,5 +11,5 @@ let rec mapTree func tree =
     |Tip(x) -> Tip(func x)
 
 [<Test>]
-let ``Adding 1 to all nodes`` () =
+let ``mod 2 to all tree`` () =
     mapTree (fun x -> x % 2) <| Tree (5, Tree (4, Tip 8, Tip 3), Tip 1) |> should equal <| Tree (1, Tree (0, Tip 0, Tip 1), Tip 1)    
