7f673aca7b200dcf7141199c79414eb6a81fd0a5
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-03-24 10:53:44 +0300

    update tests

--- 4-2/4-2/Program.fs
+++ 4-2/4-2/Program.fs
@@ -13,3 +13,7 @@ let rec mapTree func tree =
 [<Test>]
 let ``mod 2 to all tree`` () =
     mapTree (fun x -> x % 2) <| Tree (5, Tree (4, Tip 8, Tip 3), Tip 1) |> should equal <| Tree (1, Tree (0, Tip 0, Tip 1), Tip 1)    
+
+[<Test>]
+let ``divison 2`` () =
+    mapTree (fun x -> x / 2) <| Tree (6, Tree (4, Tip 8, Tip 4), Tip 4) |> should equal <| Tree (3, Tree (2, Tip 4, Tip 2), Tip 2)    
