225808922a5a11d21acb2987e88c1f89d6d9c7c6
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-03-26 16:37:22 +0300

    add

--- 4-4/4-4/Program.fs
+++ 4-4/4-4/Program.fs
@@ -1,12 +1,15 @@
-﻿let check x =
-    let rec help y n acc=
-        match n with
-        | 1 -> acc
-        | _ -> 
-            if (y % n = 0) then help y 1 false
-            else  help y (n - 1) acc
-    help x (x / 2) true
+﻿module Sequence
 
-let square = Seq.initInfinite (fun index -> index + 2)
-let primeSquare =  Seq.filter(fun x -> check x) square
-printfn "%A" primeSquare
+let isPrimeNumber number =
+    let rec help number acc isPrime =
+        match acc with
+        | 1 -> isPrime
+        | _ -> 
+            if (number % acc = 0) then help number 1 false
+            else  help number (acc - 1) isPrime
+    help number (number / 2) true
+
+let sequence = Seq.initInfinite (fun index -> index + 2)
+let primeInfiniteSequence =  Seq.filter(fun x -> isPrimeNumber x) sequence
+
+printfn "%A" primeInfiniteSequence
