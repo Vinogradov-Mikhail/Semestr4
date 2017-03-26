7ce9756044cbd50f482907c0f0feb40b194d99b5
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-03-26 14:47:20 +0300

    add test file

--- 4-3/4-3/Program.fs
+++ 4-3/4-3/Program.fs
@@ -1,4 +1,6 @@
-﻿open NUnit.Framework
+﻿module TreeCalc
+
+open NUnit.Framework
 open FsUnit
 
 type Expression =
@@ -10,18 +12,10 @@ type Expression =
 
 let rec claculate exp =
     match exp with 
-    | Number(n) -> n
+    | Number n -> n
     | Add(expr1, expr2) -> (claculate expr1) + (claculate expr2)
     | Subtraction(expr1, expr2) -> (claculate expr1) - (claculate expr2);
     | Multiply(expr1, expr2) -> (claculate expr1) * (claculate expr2)
     | Division(expr1, expr2) -> 
         if (claculate expr2) <> 0 then (claculate expr1) / (claculate expr2)
         else failwith "Divisor cannot be zero"
-        
-[<Test>]
-let ``test of tre calculating`` () = 
-    claculate <| Add(Number 5, Multiply(Number 4, Number 3)) |> should equal 17
-
-[<Test>]
-let ``test of tre calculating two`` () = 
-    claculate <| Division(Number 10, Multiply(Number 2, Number 1)) |> should equal 5
