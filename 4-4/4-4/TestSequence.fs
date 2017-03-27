f19756f133945c5fee04df40bc53f1cd66f4e8b2
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-03-27 11:32:30 +0300

    update name of arguments and add test file

--- 4-4/4-4/TestSequence.fs
+++ 4-4/4-4/TestSequence.fs
@@ -4,3 +4,14 @@ open Sequence
 open NUnit.Framework
 open FsUnit
 
+[<Test>]
+let ``contain 5?`` () = 
+    Seq.find(fun x -> x = 5) sequence |> should equal 5
+
+[<Test>]
+let ``contain 109?`` () = 
+    Seq.find(fun x -> x = 109) sequence |> should equal 109
+
+[<Test>]
+let ``contain 3571?`` () = 
+    Seq.find(fun x -> x = 3571) sequence |> should equal 3571
