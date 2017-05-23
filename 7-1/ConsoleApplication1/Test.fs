2915fe49ef07cbfa36670244ba4e26eeda799068
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-05-23 18:18:24 +0300

    add 7-1

---
+++ 7-1/ConsoleApplication1/Test.fs
@@ -0,0 +1,16 @@
+﻿module Test
+
+open Program
+open NUnit.Framework
+open FsUnit
+
+let test = 
+    bulder 3 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+
+[<Test>]
+let ``test from hwproj`` = 
+    test |> should equal 0.048
