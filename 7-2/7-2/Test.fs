b542766213c9c936eae6b641159aba0c6216ce8e
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-05-23 22:38:54 +0300

    add 7_2

---
+++ 7-2/7-2/Test.fs
@@ -0,0 +1,28 @@
+﻿module Test
+
+open Program
+open NUnit.Framework
+open FsUnit
+
+let result = 
+    calculate () {
+         let! x = "1"
+         let! y = "2"
+         let z = x + y
+         return z
+     }
+
+let res = calculate () {
+        let! x = "1"
+        let! y = "Ъ"
+        let z = x + y
+        return z
+    }
+
+[<Test>]
+let ``test hwproj one`` () =
+    result |> should equal <| Some(3)
+
+[<Test>]
+let ``test hwproj two`` () =
+    res |> should equal <| None
