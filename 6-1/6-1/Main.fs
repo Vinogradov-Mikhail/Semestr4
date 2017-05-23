183c60aeba02d9c220f6f72c1b9684378cf03872
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-05-24 00:54:56 +0300

    add 6-1

---
+++ 6-1/6-1/Main.fs
@@ -0,0 +1,32 @@
+﻿module Main
+
+open System.Collections.Generic
+open Program
+
+[<EntryPoint>]
+let main argv = 
+    let mac = { NameOfOs = "Mac"; InfectionProbability = 10 }
+    let windows = { NameOfOs = "Windows"; InfectionProbability = 80 }
+    let linux = { NameOfOs = "Linux"; InfectionProbability = 40 }
+    let mat : bool[,] = Array2D.zeroCreate 3 3
+    let createMat (mat : bool[,]) = 
+        mat.[0,0] <- false;
+        mat.[0,1] <- true;
+        mat.[0,2] <- true;
+        mat.[1,0] <- true;
+        mat.[1,1] <- false;
+        mat.[1,2] <- false;
+        mat.[2,0] <- true;
+        mat.[2,1] <- false;
+        mat.[2,2] <- false;
+        mat
+    let computers = List<Computer>()
+    let addComputers = 
+        computers.Add({ operatingSystem = windows;  Infected = false })
+        computers.Add({ operatingSystem = linux;  Infected = false })
+        computers.Add({ operatingSystem = mac;  Infected = true })
+        computers
+        
+    let network = new LocalNetwork(addComputers, createMat mat)
+    network.VirusInfection()
+    0 // возвращение целочисленного кода выхода
---
+++ 6-1/6-1/Program.fs
@@ -0,0 +1,59 @@
+﻿module Program
+
+open System
+open System.Collections.Generic
+
+type OperatingSystems = { NameOfOs: string; InfectionProbability: int }
+
+type Computer = { operatingSystem: OperatingSystems; mutable Infected: bool }
+
+type LocalNetwork(newNetOfComp : List<Computer>, mat : bool[,])=
+    let mutable rand = new Random()
+    let mutable listOfAllComputersInNet = newNetOfComp
+    let mutable matrixOfLink = mat
+
+    member this.StepOfVirusInfection() =
+            let listOfComputersInfectedInThisStep = new List<Computer>()
+            for i in 0 .. listOfAllComputersInNet.Count - 1 do
+                if (listOfAllComputersInNet.[i].Infected) then
+                    for j in 0 .. listOfAllComputersInNet.Count - 1 do
+                        if (matrixOfLink.[i,j] && (listOfAllComputersInNet.[j].Infected = false) && (listOfComputersInfectedInThisStep.Contains(listOfAllComputersInNet.[j]) = false)) then
+                            if(rand.Next(1, 100) <= listOfAllComputersInNet.[j].operatingSystem.InfectionProbability) then
+                                listOfAllComputersInNet.[j].Infected <- true
+                                listOfComputersInfectedInThisStep.Add(listOfAllComputersInNet.[j])
+
+    /// <summary>
+    /// print on console network status
+    /// </summary>
+    member this.PrintStatusInStep() =
+           for comp in listOfAllComputersInNet do
+               let mutable str = "OS:" + comp.operatingSystem.NameOfOs + " " + "Infection:" + comp.Infected.ToString()
+               Console.WriteLine(str)
+
+    /// <summary>
+    /// checking all comtures are infected
+    /// </summary>
+    /// <returns></returns>
+    member this.Check() =
+           let mutable i = 0;
+           for comp in listOfAllComputersInNet do
+               if (comp.Infected = false) then
+                   i <- i + 1
+           (i = 0)
+
+    /// <summary>
+    /// start virus infection
+    /// </summary>
+    member this.VirusInfection() =
+           let mutable i = 1;
+           Console.WriteLine("Step 0");
+           this.PrintStatusInStep();
+           while (this.Check() = false) do
+               this.StepOfVirusInfection();
+               Console.WriteLine("Step " + i.ToString())
+               this.PrintStatusInStep()
+               System.Threading.Thread.Sleep(800)
+               i <- i + 1
+           Console.WriteLine("//////////////////////////////////////////////////////////////")
+           Console.WriteLine("///////////////////ALL COMPUTERS INFECTED/////////////////////")
+           Console.WriteLine("//////////////////////////////////////////////////////////////")
