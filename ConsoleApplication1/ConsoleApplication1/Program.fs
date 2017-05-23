8485f1f0a7fecab7281c884e9209af3c8441fff0
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-05-23 18:02:52 +0300

    add

---
+++ ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -0,0 +1,7 @@
+﻿type bulder(x : int) =
+    member this.Bind (n : float, f : float -> float) =
+        f (System.Math.Round(n, x))
+
+    member this.Return (n : float) =
+        System.Math.Round(n, x)
+
