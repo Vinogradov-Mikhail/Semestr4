30effd3d647607d292819eb7f2d5df29059aea9d
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-08-25 16:48:19 +0300

    Add 5-4

---
+++ 5-4/5-4/Test.fs
@@ -0,0 +1,14 @@
+﻿module Test
+
+open NUnit.Framework
+open FsUnit
+open Program
+
+[<Test>]
+let ``test one``() = betareduction (App(Abs("a", Abs("b", App(Var "a", Var "b"))), Var "b")) |> should equal (Abs("b", App(Var("b"), Var("b"))))
+
+[<Test>]
+let ``II =  I``() = betareduction (App(Abs("x", Var "x"), Abs("x", Var "x"))) |> should equal (Abs("x", Var "x"))
+
+[<Test>]
+let `` KI = K*(only beta reduction)``() = betareduction (App(Abs("x", Abs("y", Var "x")), Abs("x", Var "x"))) |> should equal (Abs("y",Abs("x",Var "x")))
