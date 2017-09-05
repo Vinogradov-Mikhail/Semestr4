a1e58557b3eef4d6bebc5eaec09acda99b17ef88
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2017-09-05 21:13:58 +0300

    добавлено альфа преобразование для избежания коллизий

--- 5-4V2/5-4V2/Program.fs
+++ 5-4V2/5-4V2/Program.fs
@@ -5,34 +5,49 @@ type Term =
     | Abs of string * Term
     | App of Term * Term
 
-let rec betaReduction x = 
-    let rec betaApp c y = 
-        match c with
-        | App(s, p) -> App(betaApp s p, p)
-        | Abs(s, p) -> 
-                let rec betaAbs check k =
-                    match k with
-                    | Var(t) when t = check -> y
-                    | Var(t) -> Var(t)
-                    | Abs(m, n) -> Abs(m, betaAbs check n)
-                    | App(m, n) -> App(betaAbs check m, betaAbs check n)
-                match p with
-                | Var(o) when o = s -> y
-                | Var(o) -> Var(o)
-                | Abs(q, w) -> Abs(q, betaAbs s w)
-                | App(q, w) -> App(betaAbs s q, betaAbs s w)
-        | Var(k) -> App(c, y)
+let alphabet = ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "k"; "l"; "m"; "w"; "q"; "s"; "l"]
+
+let rec checkCollision x k =
+    match x with
+    | Var(s) when (s = k) -> true
+    | Abs(s, p) when (s = k) || checkCollision p k -> true  
+    | App(s, p) when (checkCollision s k) || (checkCollision p k) -> true      
+    | _ -> false
+
+let getLetter k = List.head (List.filter (fun x -> not (checkCollision k x)) alphabet)   
+
+let rename a b x = 
+    if (a = b) then 
+        getLetter x
+    else a
+
+let rec alphaReduction check x y =
+    match x with
+    | Var(o) -> Var(rename o check y)
+    | Abs(s, p) -> Abs(rename s check y, alphaReduction check p y)
+    | App(s, p) -> App(alphaReduction check s y, alphaReduction check p y)
+
+let rec betaAbs check k y =
+   match k with
+   | Var(t) when (t = check) -> y
+   | Var(t) -> Var(t)
+   | Abs(m, n) -> Abs(m, betaAbs check n y)
+   | App(m, n) -> App(betaAbs check m y, betaAbs check n y)
+
+let rec betaReduction x =     
     match x with   
     | Var(t) -> Var(t) 
     | Abs(t, s) -> 
         match s with
-            | App(m, n) -> Abs(t, betaReduction (betaApp m n))
+            | App(m, n) -> Abs(t, betaReduction (App(betaReduction m, betaReduction n)))
+            | Abs(m, n) -> Abs(t, betaReduction s)
             | _ -> Abs(t, s)
     | App(Var(y), s) -> 
         match s with
-        | App(m, n) -> App(Var(y), betaReduction (betaApp m n))
+        | App(m, n) -> App(Var(y), betaReduction (App(betaReduction m, betaReduction n)))
         | _ -> App(Var(y), s)
-    | App(s, y) -> betaReduction (betaApp s y)
-
-[<EntryPoint>]
-printfn "%A" <| betaReduction (Abs("x", App(Abs("y", Var("y")), Var("t"))))
+    | App(Abs(t, u), y) -> 
+        match y with
+        |Var(o) when (checkCollision x o) -> betaReduction (betaAbs (rename t o x) (alphaReduction o u x) y)     
+        | _ ->  betaReduction (betaAbs t u y)
+    | App(s, y) -> betaReduction (App(betaReduction s, betaReduction y))
--- 5-4V2/5-4V2/Test.fs
+++ 5-4V2/5-4V2/Test.fs
@@ -5,7 +5,7 @@ open FsUnit
 open Program
 
 [<Test>]
-let ``test one``() = betaReduction (App(Abs("a", Abs("b", App(Var "a", Var "b"))), Var "b")) |> should equal (Abs("b", App(Var("b"), Var("b"))))
+let ``test one``() = betaReduction (App(Abs("a", Abs("b", App(Var "a", Var "b"))), Var "b")) |> should equal (Abs("c", App(Var("b"), Var("c"))))
 
 [<Test>]
 let ``II =  I``() = betaReduction (App(Abs("x", Var "x"), Abs("x", Var "x"))) |> should equal (Abs("x", Var "x"))
